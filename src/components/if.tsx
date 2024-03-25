01f87a836f27b9ace9537af1b636cf80f767c5c9
Clover <2621869236@qq.com>
2024-03-25 15:49:56 +0800

    feat: banner text

---
+++ src/components/if.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+interface Props {
+  is?: boolean
+}
+
+export function If({ is, children }: Props & React.PropsWithChildren) {
+  if (is) return children
+  return null
+}
