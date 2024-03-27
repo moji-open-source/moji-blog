fee9d39aa9eacd18f3e893d980f092b1f54c2270
Clover <2621869236@qq.com>
2024-03-27 09:47:58 +0800

    integrated lint-stafe

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -1,5 +1,7 @@
-import { Inter, PT_Mono } from 'next/font/google'
+import { Inter, PT_Mono } from "next/font/google"
 
-export const inter = Inter({ subsets: ['latin',] })
+export const inter = Inter({ subsets: ["latin",] })
 
-export const pt_mono = PT_Mono({ subsets: ['latin'], weight: '400' })
+export const pt_mono = PT_Mono({ subsets: ["latin"], weight: "400" })
+
+console.log("hello world")
