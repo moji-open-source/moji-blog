abf3809c61cb1e2dc2c09b4c8433f139e285711a
Clover <2621869236@qq.com>
2024-03-27 10:39:38 +0800

    chore: add info

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -5,8 +5,6 @@ const buildEslintCommand = (filenames) =>
     .map((f) => path.relative(process.cwd(), f))
     .join(" --file ")}`
 
-console.log('hello world');
-
 module.exports = {
   "*.{js,jsx,ts,tsx}": [buildEslintCommand],
 }
