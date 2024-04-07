c54aaba8dcd9a2fd0f0e2221d0cec40e17592348
Clover <2621869236@qq.com>
2024-04-07 15:08:18 +0800

    feat: sidebar info

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,8 +1,18 @@
 import { Avatar } from "@nextui-org/avatar"
 import Image from "next/image"
+import Link from "next/link"
 import MyAvatar from "#/assets/avatar.jpg"
+import { Button } from "@nextui-org/react"
+import { MarkerIcon } from "#/components/marker"
+
+import Styles from './sidebar.module.css'
+import { OrganizationIcon } from "#/components/icons/organization-icon"
+import { GitHubIcon } from "#/components/icons/github-icon"
+
+// import MarkerSVG from '#/assets/marker.svg'
 
 export function Sidbar() {
+
   return (
     <div className="bg-white w-full rounded-md h-screen p-10 box-border shadow-md translate-y-[-410px] z-[2] sticky top-[510px]
     card-decoration-mask before:content-['']">
@@ -23,6 +33,29 @@ function Header() {
       <h5 className="text-lg font-[800] text-gray-700 mb-5">CloverYou</h5>
 
       <span className="mb-5 text-sm text-default-700">童话只美在真实却从不续写</span>
-    </div>
+      <Button color="primary" variant="faded" className="w-full mb-5" size="md">Follow</Button>
+
+      <div className="w-full">
+
+        <ul className={Styles.VcardDetails}>
+          <li className={`pl-6 text-sm pt-1 ${Styles.VcardDetail}`}>
+            <OrganizationIcon className={`float-left w-[16px] mt-1 -ml-6 text-center ${Styles.Octicon}`} />
+            <span>广州云纺科技</span>
+          </li>
+          <li className={`pl-6 text-sm pt-1 ${Styles.VcardDetail}`}>
+            <MarkerIcon className={`float-left w-[16px] mt-1 -ml-6 text-center ${Styles.Octicon}`} />
+            <span>中国-广州</span>
+          </li>
+          <li className={`pl-6 text-sm pt-1 ${Styles.VcardDetail}`}>
+            <GitHubIcon className={`float-left w-[16px] mt-1 -ml-6 text-center ${Styles.Octicon}`} />
+            <span>
+              <Link href={"https://github.com/Clover-You"} target="_blank">
+                https://github.com/Clover-You
+              </Link>
+            </span>
+          </li>
+        </ul>
+      </div>
+    </div >
   </>
 }
