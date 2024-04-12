2b472e892284435dd9025391d6a3ba0585b7cd66
Clover <2621869236@qq.com>
2024-04-12 13:11:55 +0800

    feat: mobile navbar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,20 @@
-import { Image, Navbar, NavbarBrand, NavbarContent, NavbarItem } from '@nextui-org/react'
+'use client'
+import { Image, Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenu, NavbarMenuItem, NavbarMenuToggle } from '@nextui-org/react'
 import Link from 'next/link'
 
+import React from 'react'
 import LogoImg from '#/assets/logo@185×185.webp'
 import { GitHubIcon } from '#/components/icons/github-icon'
 
 export function Header() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false)
+
   return (
-    <Navbar maxWidth="full">
+    <Navbar
+      maxWidth="full"
+      isMenuOpen={isMenuOpen}
+      onMenuOpenChange={setIsMenuOpen}
+    >
       <Link href="/" color="foreground">
         <NavbarBrand>
           <Image src={LogoImg.src} width={25} height={25} radius="none" />
@@ -29,6 +37,31 @@ export function Header() {
             <GitHubIcon className="w-[18px] text-center" fill="var(--font-color)" />
           </Link>
         </NavbarItem>
+      </NavbarContent>
+
+      <NavbarMenu className="navbar-content">
+        <NavbarMenuItem>
+          <Link color="foreground" href="/">
+            Blog
+          </Link>
+        </NavbarMenuItem>
+        <NavbarMenuItem>
+          <Link color="foreground" href="/about">
+            About
+          </Link>
+        </NavbarMenuItem>
+        <NavbarMenuItem>
+          <Link color="foreground" href="https://github.com/Clover-You" target="_blank" className="flex gap-2">
+            <GitHubIcon className="w-[18px] text-center" fill="var(--font-color)" />
+            <span>GitHub</span>
+          </Link>
+        </NavbarMenuItem>
+      </NavbarMenu>
+
+      {/* <div className="sm:hidden flex-1"></div> */}
+
+      <NavbarContent className="sm:hidden" justify="end">
+        <NavbarMenuToggle aria-label={isMenuOpen ? 'Close menu' : 'Open menu'} />
       </NavbarContent>
     </Navbar>
   )
