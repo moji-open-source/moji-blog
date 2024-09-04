b78433a35422163a4eccf1299624c90f3c9e102c
Clover You <2621869236@qq.com>
2024-09-04 10:45:31 +0800

    chore: eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,12 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu(
+  // {
+  // },
   {
     react: true,
-  },
-  {
     rules: {
-      'semi': [
-        'error',
-        'never',
-      ],
+      'semi': ['error', 'never'],
       'prefer-const': 'error',
       'react-refresh/only-export-components': 'off',
     },
