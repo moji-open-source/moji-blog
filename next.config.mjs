3ed3563f0002b8ead11f691d573b938cd86f92d0
Clover You <2621869236@qq.com>
2024-09-19 14:25:57 +0800

    refactor: slugify function to slugify.js

--- next.config.mjs
+++ next.config.mjs
@@ -3,30 +3,8 @@ import Markdown from 'unplugin-react-markdown/webpack'
 import Shiki from '@shikijs/markdown-it'
 import anchor from 'markdown-it-anchor'
 import TOC from 'markdown-it-table-of-contents'
-import { remove } from 'diacritics'
 
-const rControl = /[\u0000-\u001F]/g
-const rSpecial = /[\s~`!@#$%^&*()\-_+=[\]{}|\\;:"'<>,.?/]+/g
-
-function slugify(str) {
-  console.log(str)
-  return (
-    remove(str)
-      // Remove control characters
-      .replace(rControl, '')
-      // Replace special characters
-      .replace(rSpecial, '-')
-      // Remove continuos separators
-      .replace(/-{2,}/g, '-')
-      // Remove prefixing and trailing separtors
-      .replace(/^-+|-+$/g, '')
-      // ensure it doesn't start with a number (#121)
-      .replace(/^(\d)/, '_$1')
-      // lowercase
-      .toLowerCase()
-  )
-}
-
+import { slugify } from './scripts/slugify.js'
 
 function parseMetaString(_metaString, _code, lang) {
   return {
